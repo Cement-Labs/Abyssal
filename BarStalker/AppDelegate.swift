5f22a3cc2f1ee043426fa4251a1233dcb6873337
KrLite <68179735+KrLite@users.noreply.github.com>
2023-06-13 19:14:30 +0800

    Initial Commit

---
+++ BarStalker/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  BarStalker
+//
+//  Created by KrLite on 2023/6/13.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
